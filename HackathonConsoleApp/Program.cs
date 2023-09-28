4b7ad76ff6f7b02ffbe581f489eb76620252212d
Pawel Maczynski <pawel.maczynski@iongroup.com>
2023-09-28 11:28:28 +0200

    plan

--- HackathonConsoleApp/Program.cs
+++ HackathonConsoleApp/Program.cs
@@ -4,10 +4,22 @@
     {
         static void Main(string[] args)
         {
+            // - read conference id
             //var confNo = args[1];
 
+            // - get data from target (new) conference
             var dbClient = new DBClient();
             var conference = dbClient.Read(875).Single();
+
+            // - send data to get closest conference
+
+            // - parse response and read conference name
+
+            // - send another request to AI with conference name
+            
+            // - parse response to extract emails
+
+            // - add targets to target conference (only emails without creating a reference to PORTFOLIO table) - check if email exists
 
         }
     }
