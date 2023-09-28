d5e759cef7e84b015a99f17baf6bfb8f55f5e11d
Pawel Maczynski <pawel.maczynski@iongroup.com>
2023-09-28 11:23:00 +0200

    basic client

---
+++ HackathonConsoleApp/DBClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonConsoleApp
+{
+    public class ConferenceInfo
+    {
+        public int ConfNo { get; set; } = 0;
+
+        public string Title { get; set; } = default!;
+
+    }
+
+    internal class DBClient
+    {
+        private const string ConnectionString = "Server=WEUSHDXDB1.secure.dlgroup.com,5551\\DXSHARED;Database=dx_PIE_PIE1_main;User Id=BuildAdmin;Password=BuildAdmin;TrustServerCertificate=True";
+
+        public IEnumerable<ConferenceInfo> Read(int confNo)
+        {
+            var conference = new List<ConferenceInfo>();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand($"select [CONF_NO], [CONF_NAME] from [CONFERENCE] where [CONF_NO] = {confNo}", conn);
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var movie = new ConferenceInfo()
+                    {
+                        ConfNo = Convert.ToInt32(reader["CONF_NO"]),
+                        Title = reader["CONF_NAME"]?.ToString() ?? string.Empty,
+                    };
+
+                    conference.Add(movie);
+                }
+            }
+            return conference;
+        }
+    }
+}
---
+++ HackathonConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HackathonConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var confNo = args[1];
+
+            var dbClient = new DBClient();
+            var conference = dbClient.Read(875).Single();
+
+        }
+    }
+}
